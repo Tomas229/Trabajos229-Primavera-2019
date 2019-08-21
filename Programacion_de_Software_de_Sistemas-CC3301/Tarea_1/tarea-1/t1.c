7b8d51d8f8f50707e380f2d9e10d77d2b51dd6db
Tomas229 <tomas.cortez@ug.uchile.cl>
2019-08-20 22:03:43 -0400

    PSS: Compila y falta arreglar la función 1 y la 2

---
+++ Programacion_de_Software_de_Sistemas-CC3301/Tarea_1/tarea-1/t1.c
@@ -0,0 +1,64 @@
+// Coloque aca los #include que necesite
+// Si usa por ejemplo strcpy, ingrese en el shell de comandos:
+// $ man 3 strcpy
+// En SYNOPSIS le indicara que debe incluir el archivo de encabezados string.h
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "t1.h"
+
+// Programe aca las 3 funciones pedidas
+
+uint comprimir(uint *a, int nbits){
+
+	int lenUint=sizeof(uint)<<3;
+	
+	uint final=0;
+	
+	int mask = ~(-1<<nbits);
+	
+	for(int i = nbits; i<= lenUint; i+=nbits){
+		final = final << nbits;
+		final = final||(*a & mask);
+		a++;
+	}
+	return final;
+}
+
+char * insercion(char *d, char *s){
+
+	int len1 = strlen(d);			
+	int len2 = strlen(s);
+	int len3 = len1 + len2 + 1;
+	
+	char * a = malloc(sizeof(char)*len3);
+	
+	strcpy(a,s);
+	strcat(a,d);
+	
+
+	return a;
+}
+
+void insertar(char *d, char *s) {
+	int len1 = strlen(d);
+	int len2 = strlen(s);
+
+	char * a = d;
+
+	a = a + len1 + len2;
+	d = d + len1;
+
+	for (int i = 0; i < len1 + 1; i++) {
+		*a = *d;
+		a = a - 1;
+		d = d - 1;
+	}
+
+	for (int i = 0; i < len2; i++) {
+		*d = *s;
+		d++;
+		s++;
+	}
+}
+
