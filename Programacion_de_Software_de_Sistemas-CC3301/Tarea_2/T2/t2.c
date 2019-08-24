707433552ff0b27042d487ae7db81b879db4abd9
Tomas229 <tomas.cortez@ug.uchile.cl>
2019-08-24 16:07:20 -0400

    Ramo: PSS, Tiempo: 0:30 se inicia y se pasan los primeros tres test de la tarea 2

---
+++ Programacion_de_Software_de_Sistemas-CC3301/Tarea_2/T2/t2.c
@@ -0,0 +1,22 @@
+#include "t2.h"
+#include <stddef.h>
+
+
+
+int calzar(Nodo *a, Nodo **ppat) {
+	Nodo *b = *ppat;
+	if (b == NULL) {    //Ejemplo i)
+		*ppat = a;
+		return 1;
+	}
+
+	if (a == NULL && b != NULL) { //Ejemplo ii)
+		return 0;
+	}
+
+	if (a->x != b->x) {			//Ejemplo iii)
+		return 0;
+	}
+
+	return calzar(a, &b->izq) & calzar(a, &b->der);
+}
